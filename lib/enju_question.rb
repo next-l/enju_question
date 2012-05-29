ca983e700cab7b52b60e8afa14be1aa70191e5af
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-05-30 03:53:43 +0900

    moved get_question

--- lib/enju_question.rb
+++ lib/enju_question.rb
@@ -1,4 +1,23 @@
 require "enju_question/engine"
 
 module EnjuQuestion
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def enju_question
+      include EnjuLibrary::InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def get_question
+      @question = Question.find(params[:question_id]) if params[:question_id]
+      authorize! :show, @question if @question
+    end
+  end
 end
+
+ActionController::Base.send(:include, EnjuQuestion)
+
