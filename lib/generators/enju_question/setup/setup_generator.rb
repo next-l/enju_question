bbacdb1f32ec24d1c15e430f4321550ceebe8af1
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 02:21:09 +0900

    updated generator

--- lib/generators/enju_question/setup/setup_generator.rb
+++ lib/generators/enju_question/setup/setup_generator.rb
@@ -5,9 +5,5 @@ class EnjuQuestion::SetupGenerator < Rails::Generators::Base
     rake("enju_question_engine:install:migrations")
     inject_into_file 'app/models/user.rb',
       "  enju_question_user_model\n", :after => "enju_leaf_user_model\n"
-    inject_into_class 'app/models/manifestation.rb', Manifestation,
-      "  enju_question_manifestation_model\n"
-    inject_into_class 'app/models/item.rb', Item,
-      "  enju_question_item_model\n"
   end
 end
