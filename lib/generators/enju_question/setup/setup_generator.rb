378ffe92a6518902b2059dfffaa7fa7e6327dbc6
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 00:42:06 +0900

    added generator

---
+++ lib/generators/enju_question/setup/setup_generator.rb
@@ -0,0 +1,9 @@
+class EnjuQuestion::SetupGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def setup
+    rake("enju_question_engine:install:migrations")
+    inject_into_file 'app/models/user.rb',
+      "  enju_question_user_model\n", :after => "enju_leaf_user_model\n"
+  end
+end
