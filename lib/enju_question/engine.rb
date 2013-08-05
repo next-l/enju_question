d8b2a53c345120e31d77f38388715ed26b92a408
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-08-05 19:20:02 +0900

    updated Gemfile

--- lib/enju_question/engine.rb
+++ lib/enju_question/engine.rb
@@ -1,6 +1,8 @@
 require 'enju_core'
 require 'acts-as-taggable-on'
 require 'enju_ndl'
+require 'protected_attributes'
+require 'rails-observers'
 
 module EnjuQuestion
   class Engine < ::Rails::Engine
