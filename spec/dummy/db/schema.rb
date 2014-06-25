cbf875620a01f61616dc47f1abd7b9deb74177c3
Kosuke Tanabe <nabeta@fastmail.fm>
2014-06-25 17:14:56 +0900

    updated gemspec

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120602141129) do
+ActiveRecord::Schema.define(:version => 20140610123439) do
 
   create_table "agent_import_files", :force => true do |t|
     t.integer  "parent_id"
