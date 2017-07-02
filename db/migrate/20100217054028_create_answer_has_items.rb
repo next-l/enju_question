97d21c23a35c753b00d7a88e92060262dfe9ef88
Kosuke Tanabe <nabeta@fastmail.fm>
2017-07-02 21:05:07 +0900

    update fixture files

--- db/migrate/20100217054028_create_answer_has_items.rb
+++ db/migrate/20100217054028_create_answer_has_items.rb
@@ -1,17 +1,11 @@
 class CreateAnswerHasItems < ActiveRecord::Migration[5.0]
-  def self.up
+  def change
     create_table :answer_has_items do |t|
-      t.integer :answer_id
-      t.integer :item_id
+      t.references :answer, foreign_key: true, null: false
+      t.references :item, foreign_key: true, null: false, type: :uuid
       t.integer :position
 
       t.timestamps
     end
-    add_index :answer_has_items, :answer_id
-    add_index :answer_has_items, :item_id
-  end
-
-  def self.down
-    drop_table :answer_has_items
   end
 end
