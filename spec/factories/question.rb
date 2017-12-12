f3267669a84081a0fc8eca3b7f0eb1d622b0486b
Kosuke Tanabe <nabeta@fastmail.fm>
2017-12-13 01:35:45 +0900

    require factory_bot

--- spec/factories/question.rb
+++ spec/factories/question.rb
@@ -1,6 +1,6 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :question do |f|
     f.sequence(:body){|n| "question_#{n}"}
-    f.user_id{FactoryGirl.create(:user).id}
+    f.user_id{FactoryBot.create(:user).id}
   end
 end
