22f9fd5be9d14b38fd62df88aea7370ef8ab3abe
Kosuke Tanabe <nabeta@fastmail.fm>
2017-12-24 23:15:46 +0900

    require factory_bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ Coveralls.wear!
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-require 'factory_girl'
+require 'factory_bot'
 require 'vcr'
 require 'sunspot-rails-tester'
 
