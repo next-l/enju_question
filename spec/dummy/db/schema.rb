c6b7b89ad7650870402e25daff77bc01706ce71e
Kosuke Tanabe <nabeta@fastmail.fm>
2019-04-01 02:46:25 +0900

    update gemspec

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180709023040) do
+ActiveRecord::Schema.define(version: 2018_07_09_023040) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer "basket_id"
